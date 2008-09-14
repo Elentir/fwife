18ff4337eb0b049c22f877272120dec1e1548304
Elentir <elentir@mailoo.org>
2008-09-14 16:38:18 +0200

    improve interface

--- plugins/timeconf.c
+++ plugins/timeconf.c
@@ -37,6 +37,8 @@
 #include "common.h"
 #include "../util.h"
 
+GtkWidget *drawingmap;
+GdkPixbuf *image=NULL;
 GtkWidget *timeview;
 
 GList *zonetime = NULL;
@@ -48,7 +50,6 @@ enum
  	COLUMN_TIME_COMMENT
 };
 
-
 plugin_t plugin =
 {
 	"timeconfig",
@@ -56,7 +57,7 @@ plugin_t plugin =
 	60,
 	load_gtk_widget,
 	GTK_ASSISTANT_PAGE_CONTENT,
-	TRUE,
+	FALSE,
 	NULL,
 	prerun,
 	run,
@@ -71,11 +72,6 @@ char *desc()
 plugin_t *info()
 {
 	return &plugin;
-}
-
-int sort_zones(gconstpointer a, gconstpointer b)
-{
-	return(strcmp(a, b));
 }
 
 char* select_mode()
@@ -150,14 +146,228 @@ char* select_mode()
 	return ptr;
 }
 
+void parselatlong(char *latlong, float *latdec, float *longdec)
+{
+	int deg, min,sec;
+	char *mark = latlong+1;
+	while((*mark) != '+' && (*mark) != '-')
+	{
+		mark++;
+		if((*mark) == '\0')
+			return;
+	}
+	int longi = atoi(mark);
+	int lati = atoi(latlong); //stop on error (so stop when find '+') -> return latitude
+	if((lati>=100000) | (lati<=-100000))
+	{
+		sec = lati%100;
+		min = (lati/100)%100;
+		deg = (lati/10000);		
+	}
+	else
+	{
+		sec = 0;
+		min = lati%100;
+		deg = lati/100;
+	}
+	*latdec = ((float)deg)+((float)min/60)+((float)sec/3600);
+	
+	if((longi>=1000000) | (longi <=-1000000))
+	{
+		sec = longi%100;
+		min = (longi/100)%100;
+		deg = (longi/10000);
+	}
+	else
+	{
+		sec = 0;
+		min = longi%100;
+		deg = longi/100;	
+	}
+	*longdec = ((float)deg)+((float)min/60)+((float)sec/3600);	
+}
+
+void getcartesiancoords(char *latlong, int *xcoord, int *ycoord)
+{
+	float x,y;
+	float lat, lon;
+	float x2 = (float)gdk_pixbuf_get_width(image);
+	float y2 = (float)gdk_pixbuf_get_height(image);
+	
+	parselatlong(latlong, &lat, &lon);
+	
+	x = x2 / 2.0 + (x2 / 2.0) * lon / 180.0;
+	y = y2 / 2.0 - (y2 / 2.0) * lat / 90.0;
+			
+	*xcoord = (int)x;
+	*ycoord = (int)y;
+}
+
+//* /!\ Need free memory *//
+char* get_country(char *elem)
+{
+	char* token;
+	char *saveptr = NULL;
+	char* str = elem;
+	
+	token = strtok_r(str, "/", &saveptr);
+	return(strdup(token));		
+}
+	
+char* get_city(char *elem)
+{
+	char* token;
+	char *saveptr = NULL;
+	char* str = elem;
+	
+	token = strtok_r(str, "/", &saveptr);
+	
+	return(strdup(saveptr));		
+}
+
+GtkTreePath *find_country_path(char *country)
+{
+	GtkTreeIter iter;
+	GtkTreeView *treeview = (GtkTreeView *)timeview;
+	GtkTreeModel *model = gtk_tree_view_get_model (treeview);
+	char *name;
+
+	if(gtk_tree_model_get_iter_first (model, &iter) == FALSE)
+	{
+		return(NULL);
+	}
+	else
+	{
+		do
+		{
+			gtk_tree_model_get (model, &iter, 0, &name, -1);
+			if(!strcmp(country, name))
+				return(gtk_tree_model_get_path(model, &iter));
+			
+		} while(gtk_tree_model_iter_next(model, &iter));
+	}
+	return(NULL);
+}
+
+void selection_changed(GtkTreeSelection *selection, gpointer data)
+{
+	GtkTreeIter iter, iter_parent;
+	GtkTreeModel *model = gtk_tree_view_get_model(GTK_TREE_VIEW(timeview));
+	
+	if (gtk_tree_selection_get_selected (selection, NULL, &iter))
+	{
+		if(gtk_tree_model_iter_parent(model, &iter_parent, &iter) == FALSE)
+		{	
+			set_page_incompleted();
+			return;
+		}
+		else
+		{
+			set_page_completed();
+		}		
+	}
+	else
+	{
+		set_page_incompleted();
+	}
+	
+	gtk_widget_queue_draw(drawingmap);
+}
+
+gboolean affiche_dessin(GtkWidget *dessin, GdkEventExpose *event, gpointer data)
+{
+	GdkColormap *colormap;
+	GdkColor couleur_fond;
+	int i, xcoord, ycoord;
+	char *coords;
+	
+	GtkTreeView *treeview;
+	GtkTreeModel *model;
+	GtkTreeIter iter, iter_parent;
+	char *country, *city;
+	GtkTreeSelection *selection;
+
+	colormap = gdk_drawable_get_colormap(dessin->window);
+    
+     // On fixe la couleur de fond
+	couleur_fond.red=0xFFFF;
+	couleur_fond.green=0xFFFF;
+	couleur_fond.blue=0;	
+
+	gdk_colormap_alloc_color(colormap,&couleur_fond,FALSE,FALSE);
+	
+    // Affichage de l'image par dessus le fond vert
+	gdk_draw_pixbuf(dessin->window, dessin->style->fg_gc[GTK_WIDGET_STATE (dessin)], image, 0, 0, 0, 0,
+        gdk_pixbuf_get_width(image), gdk_pixbuf_get_height(image), GDK_RGB_DITHER_MAX, 0, 0);
+	
+	gdk_gc_set_foreground(dessin->style->fg_gc[GTK_WIDGET_STATE(dessin)],
+			      &couleur_fond);
+	
+	if(zonetime)
+	{
+		for(i=0; i<g_list_length(zonetime); i+=4)
+		{
+			coords = (char*)g_list_nth_data(zonetime, i+1);
+			getcartesiancoords(coords, &xcoord, &ycoord);
+			gdk_draw_rectangle(drawingmap->window, drawingmap->style->fg_gc[GTK_WIDGET_STATE(drawingmap)], TRUE, xcoord, ycoord, 3, 3);
+		}
+	}
+	
+	couleur_fond.red=0xFFFF;
+	couleur_fond.green=0;
+	couleur_fond.blue=0;
+	
+	gdk_colormap_alloc_color(colormap,&couleur_fond,FALSE,FALSE);
+	
+	
+	gdk_gc_set_foreground(dessin->style->fg_gc[GTK_WIDGET_STATE(dessin)],
+			      &couleur_fond);
+	
+	selection = gtk_tree_view_get_selection (GTK_TREE_VIEW (timeview));
+	treeview = gtk_tree_selection_get_tree_view (selection);
+  		
+	if (gtk_tree_selection_get_selected (selection, &model, &iter))
+	{
+		gtk_tree_model_get (model, &iter, 0, &city, -1);
+					
+		if(gtk_tree_model_iter_parent(model, &iter_parent, &iter) == FALSE)
+		{	
+			gdk_gc_set_foreground(dessin->style->fg_gc[GTK_WIDGET_STATE(dessin)],
+					      &dessin->style->black);
+			return TRUE;
+		}
+		else
+		{
+			gtk_tree_model_get (model, &iter_parent, 0, &country, -1);			
+		}
+		
+		char *total = g_strdup_printf("%s/%s",country, city);
+		
+		gint pos =  g_list_position(zonetime, g_list_find_custom(zonetime, total, cmp_str));
+		if(i>=0)
+		{
+			getcartesiancoords((char*)g_list_nth_data(zonetime, pos-1), &xcoord, &ycoord);
+			gdk_draw_rectangle(drawingmap->window, drawingmap->style->fg_gc[GTK_WIDGET_STATE(drawingmap)], TRUE, xcoord, ycoord, 3, 3);
+		}
+		FREE(total);
+				
+	}
+		
+	// On remet la couleur noir pour le texte.
+	gdk_gc_set_foreground(dessin->style->fg_gc[GTK_WIDGET_STATE(dessin)],
+			      &dessin->style->black);		
+
+	return TRUE;
+}
+
 //* Parsing file to get zones, description of each zone, etc
 int fwifetime_find()
 {
     	char ligne[256];
     	FILE * fIn;
 	char *saveptr = NULL;
-	char* token;
-	char *str2;
+	char* token, *mark;
+	char *str;
 	int j = 0;	
 
     if ((fIn = fopen(READZONE, "r")) == NULL)
@@ -166,16 +376,23 @@ int fwifetime_find()
     {
         if (ligne[0] != '#')
 	{
-		for (str2 = ligne, j=0;; j++,str2 = NULL)
+		for (str = ligne, j=0;; j++,str = NULL)
 		{
-			token = strtok_r(str2, "\t", &saveptr);
+			token = strtok_r(str, "\t", &saveptr);
 			if(token)
 			{
+				mark = token;
+				while((*mark) != '\0')
+				{
+					mark++;
+					if((*mark) == '\n')
+						(*mark) = '\0';
+				}
 				zonetime = g_list_append(zonetime, strdup(token));	
 			}
 			else if(j<4)
 			{
-				zonetime = g_list_append(zonetime, strdup(""));				
+				zonetime = g_list_append(zonetime, strdup(""));
 				break;
 			}
 			else
@@ -191,21 +408,31 @@ int fwifetime_find()
 
 GtkWidget *load_gtk_widget()
 {
-	GtkListStore *store;
+	GtkTreeStore *store;
 	GtkTreeModel *model;
 	GtkTreeViewColumn *col;
+	GtkTreeSelection *selection;
 	GtkCellRenderer *renderer;
-	GtkWidget *image, *pVbox, *pScrollbar;
+	GtkWidget *pVbox, *pScrollbar;
+	GtkWidget *hboximg;
 	
 	pVbox = gtk_vbox_new(FALSE, 5);	
+	hboximg = gtk_hbox_new(FALSE, 5);
+	
+	drawingmap=gtk_drawing_area_new();
 
-	image = gtk_image_new_from_file("images/timemap.png");
-	gtk_box_pack_start(GTK_BOX(pVbox), image, FALSE, TRUE, 0);
+	image = gdk_pixbuf_new_from_file("images/timemap.png", NULL);
+	gtk_widget_set_size_request(drawingmap, gdk_pixbuf_get_width(image), gdk_pixbuf_get_height(image));
+	
+	g_signal_connect(G_OBJECT(drawingmap),"expose_event", (GCallback)affiche_dessin, NULL);
+	
+	gtk_box_pack_start(GTK_BOX(hboximg), drawingmap, FALSE, TRUE, 150);
+	gtk_box_pack_start(GTK_BOX(pVbox), hboximg, FALSE, TRUE, 5);
 
 	//GtkWidget *separator = gtk_hseparator_new();
 	//gtk_box_pack_start (GTK_BOX (pVbox), separator, FALSE, FALSE, 5);
 	
-	store = gtk_list_store_new(3, G_TYPE_STRING, G_TYPE_STRING, G_TYPE_STRING);
+	store = gtk_tree_store_new(2, G_TYPE_STRING, G_TYPE_STRING);
 	model = GTK_TREE_MODEL(store);
 	
 	timeview = gtk_tree_view_new_with_model(model);
@@ -215,23 +442,20 @@ GtkWidget *load_gtk_widget()
 	col = gtk_tree_view_column_new();
 	renderer = gtk_cell_renderer_text_new();
 	gtk_tree_view_column_pack_start(col, renderer, TRUE);
-	gtk_tree_view_column_set_attributes(col, renderer, "text", COLUMN_TIME_CODE, NULL);
-	gtk_tree_view_column_set_title(col, "CODE");
-	gtk_tree_view_append_column(GTK_TREE_VIEW(timeview), col);
-	
-	col = gtk_tree_view_column_new();
-	renderer = gtk_cell_renderer_text_new();
-	gtk_tree_view_column_pack_start(col, renderer, TRUE);
-	gtk_tree_view_column_set_attributes(col, renderer, "text", COLUMN_TIME_NAME, NULL);
-	gtk_tree_view_column_set_title(col, "Name");
+	gtk_tree_view_column_set_attributes(col, renderer, "text", 0, NULL);
+	gtk_tree_view_column_set_title(col, "Continent/Main City");
 	gtk_tree_view_append_column(GTK_TREE_VIEW(timeview), col);
 
 	col = gtk_tree_view_column_new();
 	renderer = gtk_cell_renderer_text_new();
 	gtk_tree_view_column_pack_start(col, renderer, TRUE);
-	gtk_tree_view_column_set_attributes(col, renderer, "text", COLUMN_TIME_COMMENT, NULL);
+	gtk_tree_view_column_set_attributes(col, renderer, "text", 1, NULL);
 	gtk_tree_view_column_set_title(col, "Comments");
 	gtk_tree_view_append_column(GTK_TREE_VIEW(timeview), col);
+	
+	selection = gtk_tree_view_get_selection (GTK_TREE_VIEW (timeview));
+	
+	g_signal_connect (selection, "changed",  G_CALLBACK (selection_changed), NULL);
 
 	pScrollbar = gtk_scrolled_window_new(NULL, NULL);
 	
@@ -247,44 +471,78 @@ int prerun(GList **config)
 {
 	int i;
 	GtkTreeIter iter;
+	GtkTreeIter child_iter;
+	GtkTreePath *path = NULL;
 	
-	char *ptr = select_mode();
-	if(ptr)
-		fwtime_hwclockconf(CLOCKFILE, ptr);
+	GtkTreeView *treeview = (GtkTreeView *)timeview;
+	GtkTreeModel *model = gtk_tree_view_get_model (treeview);	
+	char *country, *city, *elem;
+	
+	fwifetime_find();
+	
 		
-	//* Add zones to the list *//
-	if(zonetime == NULL)
+	if(zonetime)
 	{
-		// search zones
-		fwifetime_find();
-		// add them to the list
 		for(i=0; i < g_list_length(zonetime); i+=4) 
 		{		
-			gtk_list_store_append(GTK_LIST_STORE(gtk_tree_view_get_model(GTK_TREE_VIEW(timeview))), &iter);
-			
-			gtk_list_store_set(GTK_LIST_STORE(gtk_tree_view_get_model(GTK_TREE_VIEW(timeview))), &iter,
-				COLUMN_TIME_CODE, (char*)g_list_nth_data(zonetime, i), COLUMN_TIME_NAME, (char*)g_list_nth_data(zonetime, i+2), COLUMN_TIME_COMMENT, (char*)g_list_nth_data(zonetime, i+3), -1);	
-		
+			elem = strdup((char*)g_list_nth_data(zonetime, i+2));
+			country = get_country(elem);
+			FREE(elem);
+			elem = strdup((char*)g_list_nth_data(zonetime, i+2));
+			city = get_city(elem);
+			FREE(elem);
+			if((path = find_country_path(country)) == NULL)
+			{
+				gtk_tree_store_append(GTK_TREE_STORE(gtk_tree_view_get_model(GTK_TREE_VIEW(timeview))), &iter, NULL);
+				gtk_tree_store_set(GTK_TREE_STORE(gtk_tree_view_get_model(GTK_TREE_VIEW(timeview))), &iter,
+					   0, country, -1);
+				gtk_tree_store_append(GTK_TREE_STORE(gtk_tree_view_get_model(GTK_TREE_VIEW(timeview))), &child_iter, &iter);
+				gtk_tree_store_set(GTK_TREE_STORE(gtk_tree_view_get_model(GTK_TREE_VIEW(timeview))), &child_iter,
+						0, city,1,(char*)g_list_nth_data(zonetime, i+3), -1);
+				
+			}
+			else
+			{
+				if(gtk_tree_model_get_iter(model, &iter, path) == FALSE)
+					return(-1); 				
+				gtk_tree_store_append(GTK_TREE_STORE(gtk_tree_view_get_model(GTK_TREE_VIEW(timeview))), &child_iter, &iter);
+				gtk_tree_store_set(GTK_TREE_STORE(gtk_tree_view_get_model(GTK_TREE_VIEW(timeview))), &child_iter,
+						0, city,1,(char*)g_list_nth_data(zonetime, i+3), -1);
+			}
+			FREE(country);
+			FREE(elem);
 		}
 	}
+	
 	return 0;
 }
 
 int run(GList **config)
 {
-	char* ret = NULL, *ptr;
-	GtkTreeIter iter;
-  	GtkTreeModel *model = gtk_tree_view_get_model (GTK_TREE_VIEW(timeview));
-  	GtkTreeSelection *selection = gtk_tree_view_get_selection (GTK_TREE_VIEW(timeview));
+	char* city = NULL, *country = NULL, *ptr=NULL;
+	GtkTreeIter iter, iter_parent;
+  	GtkTreeModel *model = gtk_tree_view_get_model(GTK_TREE_VIEW(timeview));
+  	GtkTreeSelection *selection = gtk_tree_view_get_selection(GTK_TREE_VIEW(timeview));
 
   	if (gtk_tree_selection_get_selected (selection, NULL, &iter))
         {
-  	  gtk_tree_model_get (model, &iter, COLUMN_TIME_NAME, &ret, -1);
-	  if(ret)
-		ptr = g_strdup_printf("/usr/share/zoneinfo/%s", ret);
+  	  gtk_tree_model_get (model, &iter, 0, &city, -1);
+	  
+					
+	  if(gtk_tree_model_iter_parent(model, &iter_parent, &iter) == FALSE)
+	  {	
+		 return(-1);
+	  }
+	  else
+	  {
+		  gtk_tree_model_get (model, &iter_parent, 0, &country, -1);			
+	  }
+		
+	  ptr = g_strdup_printf("%s/%s",country, city);
+	  if(ptr)
 		symlink(ptr, ZONEFILE);
-		FREE(ptr);
-    	}	
+	  FREE(ptr);
+    	 }
 	return 0;
 }
 
